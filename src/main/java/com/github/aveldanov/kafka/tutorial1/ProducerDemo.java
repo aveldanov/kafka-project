4ce470a06861900f3815863fe3c21f21bb65612b
Anton Veldanov <anton.veldanov@gmail.com>
2019-10-28 20:41:11 -0700

    kafka

--- src/main/java/com/github/aveldanov/kafka/tutorial1/ProducerDemo.java
+++ src/main/java/com/github/aveldanov/kafka/tutorial1/ProducerDemo.java
@@ -1,11 +1,33 @@
 package com.github.aveldanov.kafka.tutorial1;
 
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
 
   public static void main(String[] args) {
-    System.out.println("Hello world");
-  }
+    String bootstrapServers = "127.0.0.1:9092";
+    // create Producer properties
+    Properties properties = new Properties();
+//    properties.setProperty("bootstarp.servers", bootstrapServers);
+//    properties.setProperty("key.serializer", StringSerializer.class.getName());
+//    properties.setProperty("value.serializer", StringSerializer.class.getName());
 
+    properties.setProperty("bootstarp.servers", bootstrapServers);
+    properties.setProperty("key.serializer", StringSerializer.class.getName());
+    properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+
+
+
+    // create the producer
+
+
+    // send data
+
+
+  }
 
 
 }
