44da49cf8ea3c5a6d57f604d4bffe2ceebb2b586
Anton Veldanov <anton.veldanov@gmail.com>
2019-10-31 21:35:09 -0700

    kafka

--- src/main/java/com/github/aveldannov/kafka/kafkatwitterproject/TwitterProducer.java
+++ src/main/java/com/github/aveldannov/kafka/kafkatwitterproject/TwitterProducer.java
@@ -10,10 +10,14 @@ import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
 import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
@@ -50,6 +54,8 @@ public class TwitterProducer {
     client.connect();
     //create kafka producer
 
+    KafkaProducer<String, String> producer = createKafkaProducer();
+
 
     // loop to send tweets to kafka
     // on a different thread, or multiple different threads....
@@ -65,10 +71,10 @@ public class TwitterProducer {
         logger.info(msg);
 
       }
-//      something(msg);
-//      profit();
+      //      something(msg);
+      //      profit();
     }
-logger.info("End of application");
+    logger.info("End of application");
   }
 
 
@@ -95,6 +101,26 @@ logger.info("End of application");
     return hosebirdClient;
 
 
+  }
+
+  public KafkaProducer<String, String> createKafkaProducer() {
+
+    String bootstrapServers = "127.0.0.1:9092";
+    // create Producer properties
+    Properties properties = new Properties();
+    //    properties.setProperty("bootstarp.servers", bootstrapServers);
+    //    properties.setProperty("key.serializer", StringSerializer.class.getName());
+    //    properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+    properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+    // create the producer
+    // key - String, value - String
+    KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+    return producer;
   }
 
 }
